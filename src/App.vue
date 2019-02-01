2f4e93973b6e409fae25c5f45a2ede5d291df35f
liujiayii <liujiayiii@qq.com>
2019-02-01 11:01:55 +0800

    dist

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,14 @@
     <layout></layout>
   </div>
 </template>
+
 <script>
- import layout from './views/layout/layout.vue'
+  import layout from './views/layout/layout'
+
+
   export default {
     name: 'app',
-    components:{
-      layout
-    }
+    components: {layout}
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    height: 100%;
-  }
-
-</style>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,136 +1,136 @@
 <template>
-  <div>
-    <el-container style="height: 100%">
+  <el-container style="height: 100%">
+    <el-container>
+      <el-header class="head">
+        <div>喔驰后台管理系统</div>
+        <img src="../../assets/images/close.png"/>
+      </el-header>
       <el-container>
-        <el-header class="head">
-          <div>喔驰后台管理系统</div>
-          <img src="../../assets/images/close.png"/>
-        </el-header>
+        <el-aside style="width: auto">
+
+          <el-menu
+                  :default-active="$route.path"
+                  class="el-menu-vertical-demo"
+                  router
+                  unique-opened
+                  :collapse-transition="true">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="menu-ico"><img src="/assets/images/1.png"></i>
+                <span>客户信息</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="/CarResource">车辆资源</el-menu-item>
+                <el-menu-item index="/BuyCarConsult">买车咨询</el-menu-item>
+                <el-menu-item index="/DisplaceCar">置换车辆</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/2.png"></i>
+                <span slot="title">交易管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1">评估审核</el-menu-item>
+                <el-menu-item index="2-2">保险凭证</el-menu-item>
+                <el-menu-item index="2-3">分期管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/3.png"></i>
+                <span slot="title">售车管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="3-1">售车管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/4.png"></i>
+                <span slot="title">车辆管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="4-1">车辆管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/5.png"></i>
+                <span slot="title">财务管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="5-1">财务管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="6">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/6.png"></i>
+                <span slot="title">库存管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="6-1">库存管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="7">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/7.png"></i>
+                <span slot="title">人员管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="7-1">员工管理</el-menu-item>
+                <el-menu-item index="7-2">权限管理</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="8">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/7.png"></i>
+                <span slot="title">维修管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="8-1">维修工单</el-menu-item>
+                <el-menu-item index="8-2">维修车型分类</el-menu-item>
+                <el-menu-item index="8-3">维修工时计算</el-menu-item>
+                <el-menu-item index="8-4">维修项目定义</el-menu-item>
+                <el-menu-item index="8-5">维修项目用料</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="9">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/7.png"></i>
+                <span slot="title">维修档案管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="9-1">客户档案</el-menu-item>
+                <el-menu-item index="9-2">车辆档案</el-menu-item>
+                <el-menu-item index="9-3">员工档案</el-menu-item>
+                <el-menu-item index="9-4">驾驶员信息档案</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="10">
+              <template slot="title">
+                <i class="menu-ico"><img src="/static/images/7.png"></i>
+                <span slot="title">报表管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="10-1">维修报表</el-menu-item>
+                <el-menu-item index="10-2">售车报表</el-menu-item>
+                <el-menu-item index="10-3">进车报表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
         <el-container>
-          <el-aside style="width: auto">
-            <el-menu
-                    class="el-menu-vertical-demo"
-                    router
-                    :collapse="isCollapse"
-                    unique-opened>
-              <el-submenu index="1">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/assets/images/1.png"></i>
-                  <span>客户信息</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="CarResource">车辆资源</el-menu-item>
-                  <el-menu-item index="BuyCarConsult">买车咨询</el-menu-item>
-                  <el-menu-item index="DisplaceCar">置换车辆</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="2">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/2.png"></i>
-                  <span slot="title">交易管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="2-1">评估审核</el-menu-item>
-                  <el-menu-item index="2-2">保险凭证</el-menu-item>
-                  <el-menu-item index="2-3">分期管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="3">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/3.png"></i>
-                  <span slot="title">售车管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="3-1">售车管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="4">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/4.png"></i>
-                  <span slot="title">车辆管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="4-1">车辆管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="5">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/5.png"></i>
-                  <span slot="title">财务管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="5-1">财务管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="6">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/6.png"></i>
-                  <span slot="title">库存管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="6-1">库存管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="7">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/7.png"></i>
-                  <span slot="title">人员管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="7-1">员工管理</el-menu-item>
-                  <el-menu-item index="7-2">权限管理</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="8">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/7.png"></i>
-                  <span slot="title">维修管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="8-1">维修工单</el-menu-item>
-                  <el-menu-item index="8-2">维修车型分类</el-menu-item>
-                  <el-menu-item index="8-3">维修工时计算</el-menu-item>
-                  <el-menu-item index="8-4">维修项目定义</el-menu-item>
-                  <el-menu-item index="8-5">维修项目用料</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="9">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/7.png"></i>
-                  <span slot="title">维修档案管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="9-1">客户档案</el-menu-item>
-                  <el-menu-item index="9-2">车辆档案</el-menu-item>
-                  <el-menu-item index="9-3">员工档案</el-menu-item>
-                  <el-menu-item index="9-4">驾驶员信息档案</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-              <el-submenu index="10">
-                <template slot="title">
-                  <i class="menu-ico"><img src="/static/images/7.png"></i>
-                  <span slot="title">报表管理</span>
-                </template>
-                <el-menu-item-group>
-                  <el-menu-item index="10-1">维修报表</el-menu-item>
-                  <el-menu-item index="10-2">售车报表</el-menu-item>
-                  <el-menu-item index="10-3">进车报表</el-menu-item>
-                </el-menu-item-group>
-              </el-submenu>
-            </el-menu>
-          </el-aside>
-          <el-container>
-            <el-main>
-              <transition name="main" mode="out-in">
-                <router-view></router-view>
-              </transition>
-            </el-main>
-            <el-footer>©2018 智莱云 All rights resered 石家庄智莱云信息技术有限公司</el-footer>
-          </el-container>
+          <el-main>
+            <transition name="main" mode="out-in">
+              <router-view></router-view>
+            </transition>
+          </el-main>
+          <el-footer>©2018 智莱云 All rights resered 石家庄智莱云信息技术有限公司</el-footer>
         </el-container>
       </el-container>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script>
@@ -139,7 +139,7 @@
     name: "layout",
     data: function () {
       return {
-        navActive: '1-1',
+        navActive: '1',
         isCollapse: false,
         tableData: {
           data: [],
@@ -151,13 +151,18 @@
     },
     methods: {
       handleEdit(index, row) {
-        console.log(index, row);
-        window.location.href = 'carManageEdit.jsp?id=' + row.id
+        console.log(index, row)
       },
       handleCurrentChange(val) {
         this.getTable(val)
         console.log(val)
       },
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
     },
   }
 </script>
@@ -168,9 +173,11 @@
     transform: translateY(30px);
 
   }
+
   .main-enter-active {
     transition: all 0.2s;
   }
+
   .main-leave-active {
     position: absolute;
     transition: all 0.3s;
