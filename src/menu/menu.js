8633e0f2a52475438d065be4b99d77aa0b93e626
liujiayii <liujiayiii@qq.com>
2019-02-16 09:24:35 +0800

    1

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,47 +1,35 @@
-let menu = {};
-
-menu.home = {
+let menu = [{
   name: '首页',
   path: '/home',
   icon: 'el-icon-menu'
-}
-menu.Car = {
+}, {
   name: '客户信息',
-  children: {},
-  path: '1',
-  icon: 'el-icon-location'
-}
-let car = menu.Car.children
-car.CarResource = {
-  name: '车辆资源',
-  path: '/carResource',
-}
-car.BuyCarConsult = {
-  name: '买车咨询',
-  path: '/buyCarConsult',
-}
-car.DisplaceCar = {
-  name: '置换车辆',
-  path: '/displaceCar',
-}
-menu.Cars = {
+  icon: 'el-icon-location',
+  children: [{
+    name: '车辆资源',
+    path: '/carResource',
+  }, {
+    name: '买车咨询',
+    path: '/buyCarConsult',
+  }, {
+    name: '置换车辆',
+    path: '/displaceCar',
+  }]
+}, {
   name: '客户信息2',
-  path: '2',
-  children: {},
-  icon: 'el-icon-location'
-}
-let cars = menu.Cars.children
-cars.CarResource = {
-  name: '车辆资源2',
-  path: '/carRe',
-}
-cars.BuyCarConsult = {
-  name: '买车咨询2',
-  path: '/buyCar',
-}
-cars.DisplaceCar = {
-  name: '置换车辆2',
-  path: '/dis',
-}
+  icon: 'el-icon-location',
+  children: [
+    {
+      name: '车辆资源2',
+      path: '/carRe',
+    }, {
+      name: '买车咨询2',
+      path: '/buyCar',
+    }, {
+      name: '置换车辆2',
+      path: '/dis',
+    }
+  ]
+}]
 
 export default menu;
